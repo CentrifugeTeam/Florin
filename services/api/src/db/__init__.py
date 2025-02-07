caf524f7278d20e20dea95dbfc53ce0bdb5261a1
bevzd <dbevzpr@mail.ru>
2025-02-07 22:38:25 +0200

    step by step...

---
+++ services/api/src/db/__init__.py
@@ -0,0 +1,4 @@
+from sqlmodel import SQLModel
+from .profile import Profile
+from .tokens import ForgotPasswordToken, PermissionToken
+from .roles import Role
---
+++ services/api/src/exceptions.py
@@ -0,0 +1,16 @@
+class FileException(Exception):
+    pass
+
+
+
+
+class InvalidResetPasswordToken(Exception):
+    pass
+
+
+class UserAlreadyVerifiedException(Exception):
+    pass
+
+
+class GenerationFileException(Exception):
+    pass
