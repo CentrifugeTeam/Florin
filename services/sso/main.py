05c9070f225003a7d0498f61b826870e0972e58a
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2025-02-08 23:25:36 +0200

    add google auth

---
+++ services/sso/main.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import FastAPI
+from src import api
+
+app = FastAPI()
+app.include_router(api)
+
+
+if __name__ == '__main__':
+  uvicorn.run('__main__:app', host='0.0.0.0', port=8000, reload=True)
