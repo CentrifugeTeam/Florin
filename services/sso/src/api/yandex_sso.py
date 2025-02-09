9a8cedc6412dff553d74cd0ea848bd921af16048
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2025-02-09 14:24:11 +0200

    add sso yandex

--- services/sso/src/api/yandex_sso.py
+++ services/sso/src/api/yandex_sso.py
@@ -3,22 +3,22 @@ from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI, Request
 import logging
-from fastapi_sso.sso.google import GoogleSSO
+from fastapi_sso.sso.yandex import YandexSSO
 from fastapi import APIRouter
 
-r = APIRouter(prefix="/google")
+r = APIRouter(prefix="/yandex")
 load_dotenv()
 
-CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+CLIENT_ID = os.getenv("YANDEX_CLIENT_ID")
+CLIENT_SECRET = os.getenv("YANDEX_CLIENT_SECRET")
 DOMAIN = os.getenv("DOMAIN")
 
 app = FastAPI()
 
-sso = GoogleSSO(
+sso = YandexSSO(
   client_id=CLIENT_ID,
   client_secret=CLIENT_SECRET,
-  redirect_uri="http://127.0.0.1:8000/sso/google/callback", # DOMAIN + /sso/google/callback
+  redirect_uri="http://127.0.0.1:8000/sso/yandex/callback", # DOMAIN + /sso/yandex/callback
   allow_insecure_http=True,
 )
 
@@ -26,11 +26,11 @@ sso = GoogleSSO(
 @r.get("/login")
 async def auth_init():
   async with sso:
-    return await sso.get_login_redirect(params={"prompt": "consent", "access_type": "offline"})
+    return await sso.get_login_redirect()
 
 
 @r.get("/callback")
 async def auth_callback(request: Request):
   async with sso:
     user = await sso.verify_and_process(request)
-  return {"user": user}
+    return user
