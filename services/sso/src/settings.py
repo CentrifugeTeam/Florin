07beadbb666e9bdc8c5f1d0aa08d40bc24cee6bc
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2025-02-09 16:29:33 +0200

    add con to yc db

--- services/sso/src/settings.py
+++ services/sso/src/settings.py
@@ -1,14 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-path = Path(__file__).parent.parent.absolute() / '.env'
+path = Path(__file__).parent.parent / '.env'
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=path, extra='allow')
+    OAUTH_KEY: str
     DOCUMENT_API_ENDPOINT: str
     DOCUMENT_DATABASE_PATH: str
-    IS_DEBUG: bool = False
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     JWT_PRIVATE_KEY: str
