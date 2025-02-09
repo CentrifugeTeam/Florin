63aa2263763abfff90100913cc31d3d72b1f4daf
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2025-02-09 14:24:11 +0200

    add sso yandex

---
+++ services/sso/src/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+path = Path(__file__).parent.parent.absolute() / '.env'
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=path, extra='allow')
+    DOCUMENT_API_ENDPOINT: str
+    DOCUMENT_DATABASE_PATH: str
+    IS_DEBUG: bool = False
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    JWT_PRIVATE_KEY: str
+
+    @property
+    def sqlalchemy_url(self):
+        return f"sql+asyncydb://{self.DOCUMENT_API_ENDPOINT}{self.DOCUMENT_DATABASE_PATH}"
