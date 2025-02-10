35e9b485e30f9c01c501837e15c8282a61d6382a
bevzd <dbevzpr@mail.ru>
2025-02-10 22:08:30 +0200

    migrations

--- services/auth/authorization/main.py
+++ services/auth/authorization/main.py
@@ -1,10 +1,14 @@
-import uvicorn
 from fastapi import FastAPI
+
+from mangum.handlers.api_gateway import HTTPGateway
+from mangum import Mangum
 from src import api
 
 app = FastAPI()
 app.include_router(api)
 
+handler = Mangum(app, custom_handlers=[HTTPGateway])
 
 if __name__ == '__main__':
+  import uvicorn
   uvicorn.run('__main__:app', host='0.0.0.0', port=8000, reload=True)
