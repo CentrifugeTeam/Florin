5dfe769c00e08fc773b9391087f2d656df35bcf8
bevzd <dbevzpr@mail.ru>
2025-02-11 18:49:08 +0200

    test it

--- infra/auth.tf
+++
@@ -1,7 +0,0 @@
-data "yandex_function" "authorization" {
-  name       = "authorization"
-}
-
-# uv pip freeze > requirements.txt
-# zip -r authorization.zip * -x .venv -x uv.lock -x README.md -x pyproject.toml -x -x .*
-# mv authorization.zip infra/data/
