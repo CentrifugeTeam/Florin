fd7a6226f1b45a026439e653596772ce3b3100dd
bevzd <dbevzpr@mail.ru>
2025-02-16 00:59:47 +0200

    fix

--- services/auth/authentication/authentication/conf.py
+++ services/auth/authentication/authentication/conf.py
@@ -1,13 +1,14 @@
 from settings import Settings
-from sqlmodel.ext.asyncio.session import AsyncSession
-from ydb.aio.iam import ServiceAccountCredentials, YandexPassportOAuthIamCredentials
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel import Session
+from ydb.iam.auth import YandexPassportOAuthIamCredentials
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 settings = Settings() # type: ignore
-engine = create_async_engine(settings.sqlalchemy_url, connect_args={
+engine = create_engine(settings.sqlalchemy_url, connect_args={
     "credentials": YandexPassportOAuthIamCredentials(settings.OAUTH_KEY),
     'protocol': 'grpcs'})
 
-session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+session_maker = sessionmaker(engine, expire_on_commit=False, class_=Session)
 
 
 
--- services/auth/authentication/authentication/db.py
+++ services/auth/authentication/authentication/db.py
@@ -1,5 +1,8 @@
-from sqlmodel import SQLModel
+from uuid import UUID
+from sqlmodel import Field, SQLModel
+
 
 class User(SQLModel, table=True):
     __tablename__ = 'users'
+    id: UUID = Field(primary_key=True)
 
