fdfa31dc9cb52f65b15a17224da69f0423d8e5e6
bevzd <dbevzpr@mail.ru>
2025-02-15 17:36:22 +0200

    bucket fix

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,10 +2,10 @@ output "sa_id" {
   value = yandex_iam_service_account.sa.id
 }
 
-output "sa_access_key" {
+output "access_secret_key" {
   value = "${nonsensitive(yandex_iam_service_account_static_access_key.keys.access_key)}"
 }
-output "sa_secret_key" {
+output "secret_key" {
   value = "${nonsensitive(yandex_iam_service_account_static_access_key.keys.secret_key)}"
 }
 
