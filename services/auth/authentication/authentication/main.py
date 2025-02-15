421ea2bdefc69d2e90ebeb940752b0a023686a09
bevzd <dbevzpr@mail.ru>
2025-02-16 00:59:47 +0200

    fix

--- services/auth/authentication/authentication/main.py
+++ services/auth/authentication/authentication/main.py
@@ -1,21 +1,22 @@
 import json
-from uuid import UUID
+from db import User
+from conf import session_maker
+from sqlmodel import select
 
 def decode_token(token: str):
   pass
 
 
-def encode_token(user_id: UUID):
-  pass
-
-
 def handler(event: dict, context: dict):
-
+  stmt = select(User.id)
+  with session_maker() as session:
+    result = session.exec(stmt)
+    print(result)
   token = json.loads(event['body'])['token']
   decode_token(token)
 
 
   return {
-    'body': {**event, **context},
+    'body': {'result': result},
     'isBase64Encoded': False,
   }
