e61c0ff4f20c44f248741320bc940dd0f24a41ac
bevzd <dbevzpr@mail.ru>
2025-02-16 17:22:20 +0200

    fix

--- services/auth/authentication/authentication/main.py
+++ services/auth/authentication/authentication/main.py
@@ -1,28 +1,39 @@
 import json
+from uuid import UUID
 
-from sqlmodel import select
-from src.conf import session_maker
-from src.db import Token, User
+import jwt
+from src.conf import driver, settings
+from ydb import QuerySessionPool
 
 
 def decode_token(token: str):
-  stmt = select(Token).where(Token.token == token)
-  with session_maker() as session:
-    # TODO: test it
-    result = session.exec(stmt)
-    print(result)
+  try:
+    payload = jwt.decode(token, settings.JWT_PRIVATE_KEY, algorithms=['HS256'])
+  except:
+    raise jwt.InvalidTokenError
+  with QuerySessionPool(driver) as session:
+    user = session.execute_with_retries(
+      """
+      DECLARE $uuid AS TEXT;
+      SELECT * FROM users
+      WHERE users.id = $uuid
+      """,
+      {
+        '$uuid': UUID(payload['sub']).hex # change creating jwt token from UUID to hex
+
+      }
+
+    )
+  return user
+
+
 
 
 def handler(event: dict, context: dict):
-  stmt = select(User.id)
-  with session_maker() as session:
-    result = session.exec(stmt)
-    print(result)
   token = json.loads(event['body'])['token']
-  decode_token(token)
-
+  user = decode_token(token)
 
   return {
-    'body': {'result': result},
+    'body': user,
     'isBase64Encoded': False,
   }
