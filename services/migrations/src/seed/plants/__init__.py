8a95bc5a54ba97f4400e81620809b4e82ab7821d
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2025-02-16 18:11:52 +0200

    add plants to db and translaate data about plants

--- services/migrations/src/seed/plants/__init__.py
+++ services/migrations/src/seed/plants/__init__.py
@@ -3,8 +3,6 @@ from ..conf import session_maker
 from .plants_service import Plants
 
 async def main():
-  """ Основная функция, которая запускает процесс заполнения базы данных """
   async with session_maker() as session:
     plants = Plants(session)
     result = await plants.run()
-    print(result)
--- services/migrations/src/seed/plants/plants_service.py
+++ services/migrations/src/seed/plants/plants_service.py
@@ -6,6 +6,8 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from db import Plant, Family, Genius, Rank, Plan
 from uuid import uuid4
 import asyncio
+from sqlalchemy.future import select
+
 
 API_KEY = "SO5Ml5rY4LilkG5lRzoQBGNSUvfZ-gtlzCiENs8eVBY"
 HUGGIN_FACE_TOKEN="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"
@@ -22,50 +24,100 @@ class Plants:
     # device = "cuda" if torch.cuda.is_available() else "cpu"
     # self.pipe.to(device)
 
-  def fetch_plants(self, page=1):
-    """ Получает данные о растениях из Trefle API """
-    params = {"token": API_KEY, "page": page, "per_page": 100}
-    response = requests.get(BASE_URL, params=params)
-    return response.json().get("data", []) if response.status_code == 200 else []
+  async def fetch_plants(self, total_pages=5, per_page=20):
+    """ Получает и переводит данные о растениях из Trefle API """
+    all_plants = []
+
+    for page in range(1, total_pages + 1):
+      params = {"token": API_KEY, "page": page, "per_page": per_page}
+      response = requests.get(BASE_URL, params=params)
+
+      if response.status_code == 200:
+        all_plants.extend(response.json().get("data", []))
+      else:
+        print(f"Ошибка при получении данных: {response.status_code}")
+
+    return await self.translate_data(all_plants)
 
   async def translate_data(self, plants_data):
-    """ Асинхронно переводит названия растений на русский """
+    """ Асинхронно переводит все ключевые данные растений на русский """
+    
     async def translate_text(text):
       if text:
         translation = await self.translator.translate(text, src="en", dest="ru")
         return translation.text
       return text
 
-    tasks = [translate_text(plant.get("common_name", "")) for plant in plants_data]
-    translated_names = await asyncio.gather(*tasks)
+    tasks = []
+    for plant_data in plants_data:
+      if isinstance(plant_data, dict):
+        tasks.append(
+          asyncio.gather(
+            translate_text(plant_data.get("common_name", "")),
+            translate_text(plant_data.get("family", "")),
+            translate_text(plant_data.get("genus", "")),
+            translate_text(plant_data.get("rank", ""))
+          )
+        )
 
-    for plant, translated_name in zip(plants_data, translated_names):
-      plant["common_name"] = translated_name
+    all_translations = await asyncio.gather(*tasks)
 
+    for plant_data, translations in zip(plants_data, all_translations):
+      plant_data["common_name"], plant_data["family"], plant_data["genus"], plant_data["rank"] = translations
     return plants_data
 
   async def save_to_db(self, plants_data):
     """ Сохраняет растения в базу данных """
     for plant_data in plants_data:
-      family = Family(name=plant_data.get("family"))
-      genius = Genius(name=plant_data.get("genus"))
-      rank = Rank(name=plant_data.get("rank"))
+      family_name = plant_data.get("family")
+      genus_name = plant_data.get("genus")
+      rank_name = plant_data.get("rank")
+
+      family = await self.get_or_create(Family, name=family_name)
+      genius = await self.get_or_create(Genius, name=genus_name)
+      rank = await self.get_or_create(Rank, name=rank_name)
+
+      # План на 90 дней (это можно улучшить через ML)
       plan = Plan(day=1, action="Полив", note="Поливать раз в неделю")
 
       plant = Plant(
-        name=plant_data.get("common_name"),
+        name = plant_data.get("common_name"),
         bibliography=plant_data.get("bibliography"),
         slug=plant_data.get("slug"),
         year=plant_data.get("year"),
-        family=family,
-        genius=genius,
-        rank=rank,
+        image_url=plant_data["image_url"],
+        family_id=family.id,
+        genius_id=genius.id,
+        rank_id=rank.id,
         plan=plan,
       )
 
-      self.db.add_all([family, genius, rank, plan, plant])
+      # если имя цветка не изветна то скип
+      if not plant.name:
+        continue 
+
+      self.db.add(plan)
+      self.db.add(plant)
 
     await self.db.commit()
+
+  async def get_or_create(self, model, **kwargs):
+    instance = await self.db.execute(select(model).filter_by(**kwargs))
+    instance = instance.scalars().first()
+    
+    if instance:
+      return instance
+    
+    instance = model(**kwargs)
+    self.db.add(instance)
+    
+    try:
+      await self.db.commit()
+    except Exception:
+      await self.db.rollback()
+      raise
+    
+    return instance
 
   # def generate_images(self, plants_data):
   #   """ Генерирует изображения для растений и обновляет записи в БД """
@@ -83,8 +135,7 @@ class Plants:
 
   async def run(self):
     """ Запускает весь процесс автоматизации """
-    plants_data = self.fetch_plants()
-    plants_data = await self.translate_data(plants_data)
+    plants_data = await self.fetch_plants()
     await self.save_to_db(plants_data)
     # self.generate_images(plants_data)
     return "Завершено!"
